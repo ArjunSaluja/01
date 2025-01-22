2059b297cf1c5db7f602b4268ee716502a7cca8a
Arjun Saluja <arjunsaluja2004@gmail.com>
2025-01-22 13:13:44 +0530

    js

---
+++ src/app1.js
@@ -0,0 +1,12 @@
+import './app.css'
+function App(){
+    let a=2;
+    return(
+        <>
+        {a}
+        <h1 className='main'>HEELLO I AM COMING</h1>
+        <p style={{backgroundColor:"bisque"}}>THIS IS TUTORIAL OF REACT</p>
+        </>
+    )
+}
+export default App
